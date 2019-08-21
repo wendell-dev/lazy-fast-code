db302a4272393a9482b284e175a3d28a5bd05ba8
wendell-dev <wendell-dev@foxmail.com>
2019-08-21 16:07:44 +0800

    Initial modules by lazy-fast-code

---
+++ core/src/main/java/lazy/fast/code/core/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * lazy-fast-code-core
+ *
+ * @author wendell
+ * @date 2019-08-21 12:42
+ */
+package lazy.fast.code.core;
---
+++ generator/src/main/java/lazy/fast/code/generator/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * lazy-fast-code-generator
+ *
+ * @author wendell
+ * @date 2019-08-21 12:43
+ */
+package lazy.fast.code.generator;
