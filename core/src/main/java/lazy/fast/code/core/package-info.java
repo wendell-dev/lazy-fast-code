23ffafd16502725b7e8c11789b56a9c452779976
wendell-dev <wendell-dev@foxmail.com>
2019-08-23 17:41:55 +0800

    pom dependencies

--- core/src/main/java/lazy/fast/code/core/package-info.java
+++ core/src/main/java/lazy/fast/code/core/package-info.java
@@ -2,6 +2,5 @@
  * lazy-fast-code-core
  *
  * @author wendell
- * @date 2019-08-21 12:42
  */
 package lazy.fast.code.core;
--- generator/src/main/java/lazy/fast/code/generator/package-info.java
+++ generator/src/main/java/lazy/fast/code/generator/package-info.java
@@ -2,6 +2,5 @@
  * lazy-fast-code-generator
  *
  * @author wendell
- * @date 2019-08-21 12:43
  */
 package lazy.fast.code.generator;
