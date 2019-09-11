7994a4047cd2a9e5736a1731952d4ee9bdb2789a
wendell-dev <wendell-dev@foxmail.com>
2019-09-11 10:38:40 +0800

    junit

--- generator/src/main/java/lazy/fast/code/generator/Generator.java
+++ generator/src/main/java/lazy/fast/code/generator/Generator.java
@@ -8,51 +8,23 @@ import lazy.fast.code.generator.template.GeneratorService;
 import lazy.fast.code.generator.template.GeneratorServiceImpl;
 
 /**
- * 启动类
+ * LazyFastCode代码生成器
  *
  * @author wendell
  */
-public class Main {
-
-    public static void main(String[] args) {
-        // 模板文件根路径, 为空的时候就默认使用内置模板，如果有自定义模板就写模板绝对路径
-        Config.moduleTemplateRootPath = null;
-        Config.layoutTemplateRootPath = null;
-        Config.author = "wendell";
-        // 【必填项】项目名称
-        Config.projectName = "demo";
-        // 【必填项】项目根路径 - src目录的上层，如果不存在目录会自动创建
-        Config.projectRootPath = "D:\\workspace\\".concat(Config.projectName);
-        // 【必填项】要生成的根包名，如果不存在目录会自动创建
-        Config.basePackageName = "lazy.fast.code.demo";
-
-        // 生成项目骨架
-        generateLayout();
-
-        // 【必填项】要生成的模块名称，如 user、order
-        Config.moduleName = "user";
-        // 【必填项】数据库表全名
-        Config.tableName = "sys_user";
-        // 【必填项】类名字, 首字母大写, 以驼峰格式命名
-        Config.className = "User";
-        // 【必填项】类描述
-        Config.classDescription = "用户信息";
-
-        // 生成所有模板类
-        generateAll();
-    }
+public class Generator {
 
     /**
      * 生成项目骨架
      */
-    private static void generateLayout() {
+    public static void generateLayout() {
         new GeneratorLayout().generate();
     }
 
     /**
      * 生成所有模板类
      */
-    private static void generateAll() {
+    public static void generateAll() {
         generateEntity();
         generateRepository();
         generateService();
---
+++ generator/src/test/java/lazy/fast/code/generator/GeneratorTest.java
@@ -0,0 +1,43 @@
+package lazy.fast.code.generator;
+
+import org.junit.Test;
+
+/**
+ * LazyFastCode代码生成器
+ * 
+ * @author wendell
+ */
+public class GeneratorTest {
+
+    @Test
+    public void generate() {
+        // 模板文件根路径, 为空的时候就默认使用内置模板，如果有自定义模板就写模板绝对路径
+        Config.moduleTemplateRootPath = null;
+        Config.layoutTemplateRootPath = null;
+        // 作者签名, 如果不填写则按System.getProperty("user.name")取值
+        Config.author = "wendell";
+
+        // 【必填项】项目名称
+        Config.projectName = "demo";
+        // 【必填项】项目根路径 - src目录的上层，如果不存在目录会自动创建
+        Config.projectRootPath = "D:\\workspace\\".concat(Config.projectName);
+        // 【必填项】要生成的根包名，如果不存在目录会自动创建
+        Config.basePackageName = "lazy.fast.code.demo";
+
+        // 生成项目骨架
+        Generator.generateLayout();
+
+        // 【必填项】要生成的模块名称，如 user、order
+        Config.moduleName = "user";
+        // 【必填项】数据库表全名
+        Config.tableName = "sys_user";
+        // 【必填项】类名字, 首字母大写, 以驼峰格式命名
+        Config.className = "User";
+        // 【必填项】类描述
+        Config.classDescription = "用户信息";
+
+        // 生成所有模板类
+        Generator.generateAll();
+    }
+
+}
