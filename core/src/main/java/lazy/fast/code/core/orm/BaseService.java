4c4e5260df2c274e65af2bf4a554dd48bc24805f
wendell-dev <wendell-dev@foxmail.com>
2019-09-19 10:35:12 +0800

    update

--- core/src/main/java/lazy/fast/code/core/orm/BaseService.java
+++ core/src/main/java/lazy/fast/code/core/orm/BaseService.java
@@ -15,75 +15,86 @@ public interface BaseService<T extends BaseEntity> {
     /**
      * 保存一个实体，null的属性不会保存，会使用数据库默认值
      *
-     * @param record
-     * @return
+     * @param entity
+     *            实体对象
+     * @return 保存成功记录数
      */
-    int save(T record);
+    int save(T entity);
 
     /**
      * 根据主键更新属性不为null的值
      *
-     * @param record
-     * @return
+     * @param entity
+     *            实体对象
+     * @return 删除成功记录数
      */
-    int update(T record);
+    int update(T entity);
 
     /**
      * 根据主键字段进行删除
      *
-     * @param key
-     * @return
+     * @param id
+     *            数据主键
+     * @return 删除成功记录数
      */
-    int remove(Serializable key);
+    int remove(Serializable id);
 
     /**
      * 根据实体属性作为条件进行删除
      *
-     * @param record
-     * @return
+     * @param entity
+     *            实体对象
+     * @return 删除成功记录数
      */
-    int remove(T record);
+    int remove(T entity);
 
     /**
-     * 根据主键字段进行查询
+     * 根据主键查询一个实体
      *
-     * @param key
-     * @return
+     * @param id
+     *            数据主键
+     * @return 实体
      */
-    T get(Serializable key);
+    T get(Serializable id);
 
     /**
-     * 根据实体中的属性进行查询，只能有一个返回值，有多个结果是抛出异常
+     * 根据实体中的属性查询一个实体
      *
-     * @param record
-     * @return
+     * @param entity
+     *            实体对象
+     * @return 实体
      */
-    T get(T record);
+    T get(T entity);
 
     /**
-     * 根据实体中的属性值进行查询
+     * 根据实体中的属性值查询实体集合
      *
-     * @param record
-     * @return
+     * @param entity
+     *            实体对象
+     * @return 实体集合
      */
-    List<T> list(T record);
+    List<T> list(T entity);
 
     /**
-     * 根据实体属性和RowBounds进行分页查询
+     * 根据实体中的属性值查询实体分页集合
      * 
-     * @param record
+     * @param entity
+     *            实体对象
      * @param offset
+     *            offset
      * @param limit
-     * @return
+     *            limit
+     * @return 实体集合
      */
-    List<T> listPage(T record, int offset, int limit);
+    List<T> listPage(T entity, int offset, int limit);
 
     /**
      * 根据实体中的属性查询总数
      *
-     * @param record
-     * @return
+     * @param entity
+     *            实体对象
+     * @return 总数
      */
-    int count(T record);
+    int count(T entity);
 
 }
--- core/src/main/java/lazy/fast/code/core/orm/BaseServiceImpl.java
+++ core/src/main/java/lazy/fast/code/core/orm/BaseServiceImpl.java
@@ -35,50 +35,50 @@ public abstract class BaseServiceImpl<T extends BaseEntity> implements BaseServi
     }
 
     @Override
-    public int save(T record) {
-        return this.getBaseRepository().insertSelective(record);
+    public int save(T entity) {
+        return this.getBaseRepository().insertSelective(entity);
     }
 
     @Override
-    public int update(T record) {
-        Assert.notNull(record.getId(), "primary-key must be not null");
-        return this.getBaseRepository().updateByPrimaryKeySelective(record);
+    public int update(T entity) {
+        Assert.notNull(entity.getId(), "id must be not null");
+        return this.getBaseRepository().updateByPrimaryKeySelective(entity);
     }
 
     @Override
-    public int remove(Serializable key) {
-        Assert.notNull(key, "primary-key must be not null");
-        return this.getBaseRepository().deleteByPrimaryKey(key);
+    public int remove(Serializable id) {
+        Assert.notNull(id, "id must be not null");
+        return this.getBaseRepository().deleteByPrimaryKey(id);
     }
 
     @Override
-    public int remove(T record) {
-        return this.getBaseRepository().delete(record);
+    public int remove(T entity) {
+        return this.getBaseRepository().delete(entity);
     }
 
     @Override
-    public T get(Serializable key) {
-        return this.getBaseRepository().selectByPrimaryKey(key);
+    public T get(Serializable id) {
+        return this.getBaseRepository().selectByPrimaryKey(id);
     }
 
     @Override
-    public T get(T record) {
-        return this.getBaseRepository().selectOne(record);
+    public T get(T entity) {
+        return this.getBaseRepository().selectOne(entity);
     }
 
     @Override
-    public List<T> list(T record) {
-        return this.getBaseRepository().select(record);
+    public List<T> list(T entity) {
+        return this.getBaseRepository().select(entity);
     }
 
     @Override
-    public List<T> listPage(T record, int offset, int limit) {
-        return this.getBaseRepository().selectByRowBounds(record, new RowBounds(offset, limit));
+    public List<T> listPage(T entity, int offset, int limit) {
+        return this.getBaseRepository().selectByRowBounds(entity, new RowBounds(offset, limit));
     }
 
     @Override
-    public int count(T record) {
-        return this.getBaseRepository().selectCount(record);
+    public int count(T entity) {
+        return this.getBaseRepository().selectCount(entity);
     }
 
 }
